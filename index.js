709bd3791be37f821e7755c30416ceb29822934b
Gordey Williams <you@example.com>
2023-12-07 17:00:13 +0300

    gg

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let ss = 'gg';
+let name = " c"
+let ff = "pp";
+let th = `oo`;
+let jj = ` tt ${name}`
+let result = ff + " " + ss+ " " + th;
+let www = [
+ss, ff, th, jj
+];
+
+console.log(www.join(' ; '))
+
